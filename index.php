f96bf240b51bff10bc673bc45fe1cdf0dbc32440
Florian Stolzenhain <info@stolzenhain.net>
2019-05-29 17:32:13 +0200

    whitespace gotcha

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $strapdown_location = "/strapdown-0.2/strapdown.js";
 $url = '';
 $request_url = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
 $script_url  = (isset($_SERVER['PHP_SELF'])) ? $_SERVER['PHP_SELF'] : '';
-	
+
 // Get our url path and trim the / of the left and the right
 if($request_url != $script_url) $url = trim(preg_replace('/'. str_replace('/', '\/', str_replace('index.php', '', $script_url)) .'/', '', $request_url, 1), '/');
 
